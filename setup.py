4fd111ca17a0d3037683747d87d430c38e5662f4
Darren Lucas <me@darrenlucas.com>
2014-12-14 18:17:39 +0000

    Removing scaffolding

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.md')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
+with open(os.path.join(here, 'CHANGES.md')) as f:
     CHANGES = f.read()
 
 requires = [
@@ -16,7 +16,7 @@ requires = [
     ]
 
 setup(name='newshack',
-      version='0.0',
+      version='0.0.0',
       description='newshack',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
