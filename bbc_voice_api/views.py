7a80c07a5315239ad96295c3bb937b352cafe38c
Darren Lucas <darren.lucas@bbc.co.uk>
2014-12-15 14:02:02 +0000

    Adding images

--- bbc_voice_api/views.py
+++ bbc_voice_api/views.py
@@ -10,16 +10,36 @@ def language_parse(request):
     items = []
     news_data = response.json()
 
+    # ipad-retina can be changed and styfull to thumbnail, styhalf
+    # see https://confluence.dev.bbc.co.uk/display/newsapps/Moira+Image+Chef
+    thumbnail_image_base_url = 'http://ichef.bbci.co.uk/moira/img/ipad-retina/v2/thumbnail'
+    fullsize_image_base_url = 'http://ichef.bbci.co.uk/moira/img/ipad-retina/v2/styfull'
+
     for article in news_data['relations']:
-        if article['primaryType'] == 'bbc.mobile.news.item':
-            content = article['content']
+        content = article['content']
+        if content['format'] == 'bbc.mobile.news.format.textual':
+            image_id = find_first_image(content['relations'])
             items.append({
                 'name': content['name'],
                 'shortName': content['shortName'],
                 'summary': content['summary'],
-                'lastUpdated': content['lastUpdated']
+                'lastUpdated': content['lastUpdated'],
+                'images': {
+                    'fullsize': fullsize_image_base_url + image_id,
+                    'thumbnail': thumbnail_image_base_url + image_id
+                }
             })
 
     return {
         'list': items
     }
+
+
+def find_first_image(data):
+    image_id = None
+    for item in data:
+        if item['primaryType'] == 'bbc.mobile.news.image':
+            image_id = item['content']['id']
+            break
+
+    return image_id
