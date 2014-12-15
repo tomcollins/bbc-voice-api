ee00c08b20b4faea2f76c6568febc9baae84ab62
Darren Lucas <darren.lucas@bbc.co.uk>
2014-12-15 13:35:02 +0000

    Picking out some content from news api

--- bbc_voice_api/views.py
+++ bbc_voice_api/views.py
@@ -1,5 +1,25 @@
 from pyramid.view import view_config
 
+import requests
+
 @view_config(route_name='news', renderer='json')
 def language_parse(request):
-    return {'project': 'newshack'}
+
+    response = requests.get('http://newsapps-trevor-producer.cloud.bbc.co.uk/content/cps/news/front_page')
+
+    items = []
+    news_data = response.json()
+
+    for article in news_data['relations']:
+        if article['primaryType'] == 'bbc.mobile.news.item':
+            content = article['content']
+            items.append({
+                'name': content['name'],
+                'shortName': content['shortName'],
+                'summary': content['summary'],
+                'lastUpdated': content['lastUpdated']
+            })
+
+    return {
+        'list': items
+    }
